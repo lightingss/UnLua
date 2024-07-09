0304b6b03a898db5f109863fab6e2a3261ee32b5
lightings <easysl@msn.com>
2024-07-09 18:53:57 +0800

    run on 5.4

--- Source/TPSProjectEditor.Target.cs
+++ Source/TPSProjectEditor.Target.cs
@@ -15,9 +15,18 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class TPSProjectEditorTarget : TargetRules{    public TPSProjectEditorTarget(TargetInfo Target) : base(Target)    {        Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
-        ExtraModuleNames.AddRange(            new string[]            {
+public class TPSProjectEditorTarget : TargetRules
+{
+    public TPSProjectEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        ExtraModuleNames.AddRange(
+            new string[]
+            {
                 "TPSProject"
-            }            );
-    }}
+            }
+            );
+    }
+}
--- Source/TPSProjectServer.Target.cs
+++ Source/TPSProjectServer.Target.cs
@@ -19,7 +19,8 @@ public class TPSProjectServerTarget : TargetRules
     public TPSProjectServerTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Server;
-        DefaultBuildSettings = BuildSettingsVersion.V2;
+        DefaultBuildSettings = BuildSettingsVersion.V5;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
         ExtraModuleNames.Add("TPSProject");
     }
 }
